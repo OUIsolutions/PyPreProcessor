8a2fc175c1d102bccde40dfaa5bae7cac01f209d
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-02 04:31:28 -0300

    at

--- src/action.py
+++ src/action.py
@@ -7,7 +7,7 @@ class Action:
         self._name = name
 
     def call(self,callback_args:list,args:dict):
-        self.action(callback_args,args)
+        return self.action(callback_args,args)
 
     def __len__(self):
         return len(self._name)
