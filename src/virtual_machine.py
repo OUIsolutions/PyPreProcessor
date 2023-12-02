3244a6830e693e75437794c694dbfc04cae6969e
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-02 03:04:28 -0300

    att

--- src/virtual_machine.py
+++ src/virtual_machine.py
@@ -3,21 +3,42 @@
 class VirtualMachine:
 
     def __init__(self) -> None:
-        self.stack = []
-        self.code_line = 0
-        self.lines = []
-    
-    
-
-    def line_interpreter(self,line:str):
-        pass 
-
-    def exec(self,content:str):
-        pass 
+        self.stack = [
+            []
+        ]            
+        self.stack_point = 0
+        self.instruction_point = 0
+        self.instructions = []
     
 
-    def exec_file(self,file:str):
-        with open(file,'r') as arq:
-            self.exec(file)
 
+    def _instruction_declare(values:list):
+        pass
+
+
+    def _syscall(self):
+        try:
+            current_instruction = self.instructions[self.instruction_point]
+        except IndexError:
+            raise IndexError(self.instruction_point)
+
+        try:
+            action =current_instruction[0]
+            args = current_instruction[1::]
+        except IndexError:
+            raise IndexError("instruction not formated")
+
+        action_function =  self.__getattribute__(f'_instruction_{action}')
+        action_function(args)
+
+
+
+    def exec(self,content:list):
+        for r in content:
+            self.instructions.append(r)
+            self._syscall()  
+            self.instruction_point+=1
+
+
+    
     
---
+++ teste.py
@@ -0,0 +1,13 @@
+
+
+from src.virtual_machine import VirtualMachine
+
+
+
+v = VirtualMachine()
+
+
+v.exec([
+
+    ["declare","r",10]
+])
