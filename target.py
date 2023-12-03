27a0fe220b4c9a40dd4d777328ca89866fe63e0e
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-03 19:51:56 -0300

    at

--- target.py
+++ target.py
@@ -5,11 +5,13 @@ r = 30
 
 
 
-#comp: self.include('a.py')
 #comp: if self.t ==  20:
 r = 40 
 class Soma:
     def __init__(self) -> None:
+        pass 
+    #comp: self.include('a.py')
+
     #comp: if self.r == 20:
         r == #comp: self.ref(self.a)
         x:str = #comp: self.embed('a.txt')
