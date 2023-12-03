42d6ddfd6c166a132f9bb75cae82bb3abbbe2d3c
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-03 20:09:52 -0300

    at

--- first/code_block.py
+++ first/code_block.py
@@ -4,6 +4,8 @@ from .line import Line
 class CodeBlock(Line):
 
     def __str__(self) -> str:
-        if self._content == '':
+        formated_conent = self._content.strip()
+
+        if formated_conent == '':
             return ''
-        return f'{self._ident_text}{self._content}'
+        return f'{self._ident_text}{formated_conent}'
