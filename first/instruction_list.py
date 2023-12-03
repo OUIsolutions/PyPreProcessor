144c3a5a65ea5389680abb5143d429d60b416e0c
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-03 20:21:09 -0300

    at

--- first/instruction_list.py
+++ first/instruction_list.py
@@ -15,12 +15,14 @@ class InstructionList:
 
     def increase_ident(self):
         self._ident_level+=1
-  
+        #last_instruction_constructor:Callable = self._instructions[-1].__class__
+        #self._instructions.append(last_instruction_constructor(self._ident_level))
 
 
     def decrease_ident(self):
         self._ident_level-=1
-
+        #last_instruction_constructor:Callable = self._instructions[-1].__class__
+        #self._instructions.append(last_instruction_constructor(self._ident_level))
 
     def add_text_to_last_instruction(self,char:str):
         self._instructions[-1].add_text(char)
