4d767e495db486880ba6de3ce16413e9d9192de4
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-03 18:42:50 -0300

    att

--- first/pre_processor.py
+++ first/pre_processor.py
@@ -10,6 +10,11 @@ class PreProcessor:
         self.endscope = '#end'
         self.end_comptimes = ['\n', '#']
         self._text = ''
+        self._instructions:InstructionList = None
+        self._point:int  = None
+        self._content:str = None 
+        self._inside_comptime:bool = None
+        self._current_char:str = None
         # args
         self.t = 20
         self.r = 30
@@ -28,56 +33,63 @@ class PreProcessor:
                 return i
         return None
 
-    def handler_normal_text(self,content:str,current_char:str,instructions:InstructionList):
-        is_start_comptime_identfier =self.is_string_from_point(content, i, self.identifier)
+    def handler_normal_text(self):
+        is_start_comptime_identfier =self.is_string_from_point(self._content, self._point, self.identifier)
+    
         if is_start_comptime_identfier:
-            instructions.add_code_block()
-            inside_comptime = True
-            i += len(self.identifier)
-
-        instructions.add_text_to_last_instruction(current_char)
+            self._instructions.add_code_block()
+            self._inside_comptime = True
+            self._point += len(self.identifier)
+            return
         
+        self._instructions.add_text_to_last_instruction(self._current_char)
+        self._point+=1
+
+
+    def handler_comptime_text(self):
+        end_char = self.get_expected_if_is_one_of_expecteds(self._content, self._point, self.end_comptimes)
+        if end_char:
+            self._instructions.add_text_block()
+            self._inside_comptime = False
+            self._point += len(end_char)
+            return 
+
+        is_start_scope = self.is_string_from_point(self._content, self._point, self.start_scope)
+        if is_start_scope:
+            self._instructions.increase_ident()
+
 
     def compile(self, content: str) -> str:
      
-        instructions = InstructionList()
-
-        inside_comptime = False
-        i = 0
+        self._instructions  = InstructionList()
+        self._point = 0
+        self._content =content
+        self._inside_comptime = False
+        self._current_char = ''
         while True:
-            if i >= len(content):
-                return str(instructions)
 
-            current_char = content[i]
+            if self._point >= len(self._content):
+                return str(self._instructions)
 
-            is_the_end_scope = self.is_string_from_point(content, i, self.endscope)
+            self._current_char = self._content[self._point]
+
+            is_the_end_scope = self.is_string_from_point(self._content, self._point, self.endscope)
             if is_the_end_scope:
-                i+=len(self.endscope)
-                instructions.decrease_ident()
+                self._point+=len(self.endscope)
+                self._instructions.decrease_ident()
                 continue
 
 
-            is_a_normal_text = not inside_comptime
+            is_a_normal_text = not self._inside_comptime
             if is_a_normal_text:
-                self.handler_normal_text(content,current_char,instructions)
+                self.handler_normal_text()
+                
+            if self._inside_comptime:
+                self.handler_comptime_text()
 
 
-            if inside_comptime:
-                end_char = self.get_expected_if_is_one_of_expecteds(content, i, self.end_comptimes)
-                if end_char:
-                    instructions.add_text_block()
-                    inside_comptime = False
-                    i += len(end_char)
-                    continue
-
-                is_start_scope = self.is_string_from_point(content, i, self.start_scope)
-                if is_start_scope:
-                    instructions.increase_ident()
             
 
-
-            instructions.add_text_to_last_instruction(current_char)
-            i += 1
 
     def include(self, file: str):
         with open(file, 'r') as arq:
