3d36f26f9b5ee40b00398718ce1e78cd713ffcda
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-04 05:59:11 -0300

    at

--- html.py
+++ html.py
@@ -20,8 +20,11 @@ p = PreProcessor()
 p.users =[
     {"name":"test1","age":25,"married":False},
     {"name":"test2","age":26,"married":False},
-    {"name":"test3","age":27,"married":False}
+    {"name":"test3","age":27,"married":False},
+    {"name":"test4","age":27,"married":False}
+
 ]
+
 
 result = p.run(TEMPLATE)
 with open(OUTPUT,'w') as arq:
