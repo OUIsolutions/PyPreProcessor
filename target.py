7c75ec0bff7adde44f9a22cf9ff443deef972928
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-03 21:01:43 -0300

    at

--- target.py
+++ target.py
@@ -6,15 +6,18 @@ r = 30
 
 
 #comp: 
-if self.t ==  20: #>> 
-    
+if self.t ==  20:  #>> 
 #end
 r = 40 
+
 class Soma:
     def __init__(self) -> None:
         pass
 
-    #comp: if self.t ==  20: #>> #end
+    #comp:
+    if self.t ==  20:  #>>     
+    #end
+
         r == '#comp: self.ref(self.a)   #end'
         v = 30
     
