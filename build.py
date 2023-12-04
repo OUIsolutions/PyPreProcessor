80270b1db3d15991ed7d0ca1a8cd5773f82533d7
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-04 04:42:11 -0300

    at

--- build.py
+++ build.py
@@ -1,11 +1,28 @@
 from shutil import copy 
 from os import system
-system('python3 PyPreProcessor.py  main.py PyPreProcessor.py ')
-copy('PyPreProcessor.py','internal_exemples/PyPreProcessor.py')
-
+from os import listdir
 from PyPreProcessor import PreProcessor
 
-p = PreProcessor()
+system('python3 PyPreProcessor.py  main.py PyPreProcessor.py ')
+
+copy('PyPreProcessor.py','internal_exemples/PyPreProcessor.py')
+
+main_processor = PreProcessor()
+
+exemples = listdir('internal_exemples')
+for e in exemples:
+    current = f'internal_exemples/{e}'
+    p = PreProcessor()
+    p.internal = True 
+    data = {
+        'file':p.run(current),
+    }
+    p.internal = False 
+    code_to_execute = p.run(current)
+    print(code_to_execute)
+    
+    
+  
 
 system('python3 exemples/sql.py')
 
