54740c20f4572387a1b6dd84724b349338764f28
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-04 05:51:59 -0300

    at

--- html.py
+++ html.py
@@ -4,7 +4,7 @@
 from PyPreProcessor import PreProcessor
 
 
-TEMPLATE = 'templates/insert.sql'
+TEMPLATE = 'templates/index.html'
 
 #comp: if not self.internal: #>>#end 
 OUTPUT = "query.sql"
@@ -18,8 +18,8 @@ OUTPUT = "out.html"
 
 p = PreProcessor()
 p.users =[
-    {"name":"test1","age":25,"married":False}
-    {"name":"test2","age":26,"married":False}
+    {"name":"test1","age":25,"married":False},
+    {"name":"test2","age":26,"married":False},
     {"name":"test3","age":27,"married":False}
 ]
 
--- teste.py
+++
@@ -1,7 +0,0 @@
-from src import PreProcessor
-
-
-p = PreProcessor()
-p.internal = False
-r = p.run('sql.py')
-print(r)
