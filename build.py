bcf3eb9da0e5ecfaeacf06ba74475928bd554971
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-04 04:53:45 -0300

    at

--- build.py
+++ build.py
@@ -3,26 +3,30 @@ from os import system
 from os import listdir
 from PyPreProcessor import PreProcessor
 
+#boostraping itself 
 system('python3 PyPreProcessor.py  main.py PyPreProcessor.py ')
 
 copy('PyPreProcessor.py','internal_exemples/PyPreProcessor.py')
 
 main_processor = PreProcessor()
+main_processor.files = {}
 
 exemples = listdir('internal_exemples')
 for e in exemples:
     current = f'internal_exemples/{e}'
+    if e == 'PyPreProcessor.py':
+        continue
+
     p = PreProcessor()
-    p.internal = True 
+    p.internal = False 
     data = {
         'file':p.run(current),
     }
-    p.internal = False 
-    code_to_execute = p.run(current)
-    print(code_to_execute)
+    system(f'python3 {current}')
     
-    
+
+
   
 
-system('python3 exemples/sql.py')
+#system('python3 exemples/sql.py')
 
--- internal_exemples/sql.py
+++ internal_exemples/sql.py
@@ -3,16 +3,21 @@
 
 from PyPreProcessor import PreProcessor
 
+
+
+#comp:if not self.internal: #>> 
+TEMPLATE = 'templates/insert.sql'
+OUTPUT = "query.sql"
+#<< #end 
+
+
 #comp:if self.internal: #>> 
 TEMPLATE = '../templates/insert.sql'
 OUTPUT = "out.txt"
 #<< #end 
 
-#comp:if not self.internal: #>> 
-TEMPLATE = 'templates/insert.sql'
-OUTPUT = "query.sql"
 
-#<< #end 
+
 
 p = PreProcessor()
 p.username = 'Username'
