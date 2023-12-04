eeb84277bd76edd7947583afe8faa9b9d03a90aa
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-04 04:13:20 -0300

    at

--- exemples/sql.py
+++ exemples/sql.py
@@ -1,15 +1,23 @@
 
+from PyPreProcessor import PreProcessor
 
 #if self.internal: >> 
-TEMPLATE_FOLDER = '../templates'
+TEMPLATE = '../templates/insert.sql'
 OUTPUT = "out.txt"
 #end 
 
 #if not self.internal: >> 
-TEMPLATE_FOLDER = 'templates'
+TEMPLATE = 'templates/insert.sql'
 OUTPUT = "query.sql"
 #end 
 
+p = PreProcessor()
+p.username = 'Username'
+p.email = 'myemail@gamil.com'
+p.password = '123'
+p.is_root = True
 
-
-
+result = p.run(TEMPLATE)
+with open(OUTPUT,'w') as arq:
+    arq.write(result)
+    
