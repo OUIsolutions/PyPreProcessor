f01a78515cd1af6365dbbd678cd308543a07ed8b
mateusmoutinho <mateusmoutinho01@gmail.com>
2023-12-04 04:14:19 -0300

    at

--- build.py
+++ build.py
@@ -2,5 +2,4 @@ from shutil import copy
 from os import system
 
 copy('PyPreProcessor.py','exemples/PyPreProcessor.py')
-system('cd exemples')
-system('python3 sql.py')
+system('python3 exemples/sql.py')
